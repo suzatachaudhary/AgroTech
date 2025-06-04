24d925e104e911b67ddf1fa5d346b6aada347d19
Kabeeta Chaudhary <suzatac3@gmail.com>
2025-06-04 08:27:18 +0530

    Update database.py

--- AGRITECH/database.py
+++ AGRITECH/database.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Arnab@2004",
-        database="maidapp"
+        password="#########",
+        database="Agrotech"
     )
